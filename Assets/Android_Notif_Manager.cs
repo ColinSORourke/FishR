32d145b15222826a6fd2ecbef56ce3f8fd597fe7
ColinSORourke <csorourk@ucsc.edu>
2022-02-12 17:25:52 -0800

    Android Notifs auto cancel

--- Assets/Android_Notif_Manager.cs
+++ Assets/Android_Notif_Manager.cs
@@ -35,6 +35,7 @@ public class Android_Notif_Manager : NotificationManager
         var notificationA = new AndroidNotification();
         notificationA.Title = "A Bite!";
         notificationA.Text = "You caught something! Check in in the next " + duration.Minutes + " minutes to claim it!";
+        notificationA.ShouldAutoCancel = true;
         notificationA.FireTime = System.DateTime.Now + fromNow;
 
         AndroidNotificationCenter.SendNotification(notificationA, myChannel.Id);
@@ -42,6 +43,7 @@ public class Android_Notif_Manager : NotificationManager
         var notificationB = new AndroidNotification();
         notificationB.Title = "Too Slow!";
         notificationB.Text = "You caught something, but it got away! Better luck next time.";
+        notificationB.ShouldAutoCancel = true;
         notificationB.FireTime = System.DateTime.Now + fromNow + duration;
 
         var missID = Convert.ToString( AndroidNotificationCenter.SendNotification(notificationB, myChannel.Id) );
